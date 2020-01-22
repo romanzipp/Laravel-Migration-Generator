6542c774b7a8246d6c638d9e09f17c3f357b2eb3
Roman Zipp <ich@ich.wtf>
2020-01-22 23:15:29 +0100

    Add base package provider

---
+++ src/Providers/MigrationGeneratorProvider.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace romanzipp\MigrationGenerator\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use romanzipp\MigrationGenerator\Console\Commands\GenerateMigrationsCommand;
+
+class MigrationGeneratorProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            dirname(__DIR__) . '/../config/migration-generator.php' => config_path('migration-generator.php'),
+        ], 'config');
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->mergeConfigFrom(
+            dirname(__DIR__) . '/../config/migration-generator.php', 'migration-generator'
+        );
+
+        $this->commands([
+            GenerateMigrationsCommand::class,
+        ]);
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return [];
+    }
+}
