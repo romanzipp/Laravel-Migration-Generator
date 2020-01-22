61db69fa5bd332ac633185e06464c016bb21284d
Roman Zipp <ich@ich.wtf>
2020-01-22 23:18:42 +0100

    Add test case

---
+++ tests/TestCase.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace romanzipp\MigrationGenerator\Tests;
+
+use Illuminate\Database\Connection;
+use Illuminate\Database\MigrationServiceProvider;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Foundation\Application;
+use Orchestra\Testbench\TestCase as BaseTestCase;
+
+abstract class TestCase extends BaseTestCase
+{
+    /**
+     * Setup the test environment.
+     *
+     * @return void
+     */
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->setUpDatabase($this->app);
+
+        $this->artisan('migrate')->run();
+    }
+
+    /**
+     * Get package providers.
+     *
+     * @param \Illuminate\Foundation\Application $app
+     *
+     * @return array
+     */
+    protected function getPackageProviders($app)
+    {
+        return [
+            MigrationServiceProvider::class,
+        ];
+    }
+
+    /**
+     * Get the database connection.
+     *
+     * @return mixed
+     */
+    protected function db(): Connection
+    {
+        return $this->app['db']->connection();
+    }
+
+    /**
+     * Create test database tables.
+     *
+     * @param \Illuminate\Foundation\Application $app
+     */
+    protected function setUpDatabase(Application $app)
+    {
+        $app['db']->connection()->getSchemaBuilder()->create('basic_table_incrementing_timestamps', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+
+        $app['db']->connection()->getSchemaBuilder()->create('varchar_table', function (Blueprint $table) {
+            $table->string('string');
+            $table->string('nullable_string');
+            $table->string('string_length_20', 20);
+        });
+    }
+}
