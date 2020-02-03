67b7894378f11e0b434d3291838b5a4de4371212
Roman Zipp <ich@ich.wtf>
2020-02-03 23:33:03 +0100

    Add migration file creation test

---
+++ tests/FileStorageConductorTest.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace romanzipp\MigrationGenerator\Tests;
+
+use romanzipp\MigrationGenerator\Services\Conductors\ColumnsConductor;
+use romanzipp\MigrationGenerator\Services\Conductors\FileStorageConductor;
+use romanzipp\MigrationGenerator\Services\Conductors\MigrationGeneratorConductor;
+use romanzipp\MigrationGenerator\Services\Conductors\TablesConductor;
+use romanzipp\MigrationGenerator\Services\MigrationGeneratorService;
+
+class FileStorageConductorTest extends TestCase
+{
+    const OUTPUT = __DIR__ . '/Support/files';
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $files = scandir(self::OUTPUT);
+
+        foreach ($files as $file) {
+
+            $filePath = self::OUTPUT . '/' . $file;
+
+            if (pathinfo($filePath, PATHINFO_EXTENSION) !== 'php') {
+                continue;
+            }
+
+            unlink($filePath);
+        }
+    }
+
+    public function testStoringFile()
+    {
+        config(['migration-generator.path' => __DIR__ . '/Support/files']);
+
+        $migrations = [];
+
+        $service = app(MigrationGeneratorService::class);
+
+        $connection = $service->getDatabaseConnection();
+
+        $tables = (new TablesConductor($connection))->getTables();
+
+        foreach ($tables as $table) {
+            /** @var string $table */
+
+            /** @var \Doctrine\DBAL\Schema\Column[] $columns */
+            $columns = (new ColumnsConductor($connection, $table))->getColumns();
+
+            $migrations[] = (new MigrationGeneratorConductor($table, $columns))();
+        }
+
+        (new FileStorageConductor($migrations))();
+
+        foreach ($migrations as $migration) {
+            /** @var MigrationGeneratorConductor $migration */
+
+            $this->assertFileExists(
+                __DIR__ . '/Support/files/' . $migration->getFileName()
+            );
+        }
+    }
+}
