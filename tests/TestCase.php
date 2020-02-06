5176c78af8a054145a54c72561cec7d7171e3039
Roman Zipp <ich@ich.wtf>
2020-02-07 00:56:32 +0100

    Add testcase available laravel schema docs methods

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -57,6 +57,66 @@ abstract class TestCase extends BaseTestCase
     /**
      * Create test database tables.
      *
+     * bigIncrements
+     * bigInteger
+     * binary
+     * boolean
+     * char
+     * date
+     * dateTime
+     * dateTimeTz
+     * decimal
+     * double
+     * enum
+     * float
+     * geometry
+     * geometryCollection
+     * increments
+     * integer
+     * ipAddress
+     * json
+     * jsonb
+     * lineString
+     * longText
+     * macAddress
+     * mediumIncrements
+     * mediumInteger
+     * mediumText
+     * morphs
+     * uuidMorphs
+     * multiLineString
+     * multiPoint
+     * multiPolygon
+     * nullableMorphs
+     * nullableUuidMorphs
+     * nullableTimestamps
+     * point
+     * polygon
+     * rememberToken
+     * set
+     * smallIncrements
+     * smallInteger
+     * softDeletes
+     * softDeletesTz
+     * string
+     * text
+     * time
+     * timeTz
+     * timestamp
+     * timestampTz
+     * timestamps
+     * timestampsTz
+     * tinyIncrements
+     * tinyInteger
+     * unsignedBigInteger
+     * unsignedDecimal
+     * unsignedInteger
+     * unsignedMediumInteger
+     * unsignedSmallInteger
+     * unsignedTinyInteger
+     * uuid
+     * year
+     *
      * @param \Illuminate\Foundation\Application $app
      */
     protected function setUpDatabase(Application $app)
