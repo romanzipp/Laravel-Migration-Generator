040bf2d63456168a2ed39dd4c90ccef9137144eb
Roman Zipp <ich@ich.wtf>
2020-03-03 15:28:36 +0100

    Add migration column method simplifier

--- src/Services/Objects/MigrationColumnMethod.php
+++ src/Services/Objects/MigrationColumnMethod.php
@@ -10,7 +10,31 @@ class MigrationColumnMethod
 
     public function __construct(string $name, array $parameters = [])
     {
+        [$name, $parameters] = self::simplify($name, $parameters) ?? [$name, $parameters];
+
         $this->name = $name;
         $this->parameters = $parameters;
+    }
+
+    private static function simplify(string $name, array $parameters): ?array
+    {
+        if ($name === 'bigInteger') {
+
+            if (count($parameters) < 3) {
+                return null;
+            }
+
+            // Auto increment & unsigned
+            if ($parameters[1] === true && $parameters[2] === true) {
+                return [
+                    'bigIncrements',
+                    [
+                        $parameters[0],
+                    ],
+                ];
+            }
+        }
+
+        return [$name, $parameters];
     }
 }
